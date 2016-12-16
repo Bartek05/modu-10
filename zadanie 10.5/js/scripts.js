833ea661a43323c888eca923e38f9eec07b746d6
Michał Dolny <michal.dolny@getresponse.com>
2016-12-16 08:05:21 +0100

    poprawa wciec

--- zadanie 10.5/js/scripts.js
+++ zadanie 10.5/js/scripts.js
@@ -8,9 +8,9 @@ Button.prototype = {
 		this.$element.text(this.text);
 		this.$element.click(function() {
 			alert(self.text);
-	});
+        });
 		this.$element.appendTo($('body'));
 	}
 }
 var btn1 = new Button('Hello!');
-	btn1.create(); 
+btn1.create();
